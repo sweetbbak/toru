8580c4cf25733e6f642496c10105784d32f022cc
Meili C <sweetbabyalaska@gmail.com>
2024-03-02 14:30:56 -0900

    new dev nix pkg update

--- package.nix
+++ package.nix
@@ -1,40 +1,47 @@
-{ lib, fetchFromGitHub, buildGoModule }:
+{
+  lib,
+  fetchFromGitHub,
+  buildGoModule,
+}:
 
 buildGoModule rec {
-    pname = "toru";
-    version = "0.1";
+  pname = "toru";
+  version = "0.1";
 
-    src = fetchFromGitHub {
-      owner = "sweetbbak";
-      repo = "toru";
-      rev = "9dc67d420208bb5f9debd260170d54035242c7ab";
-      hash = "sha256-2Z5agQtF6p21rnAcjsRr+3QOJ0QGveKVH8e9LHpm3ZE=";
-    };
+  src = fetchFromGitHub {
+    owner = "sweetbbak";
+    repo = "toru";
+    rev = "c5f6134b42d6dc8fd3f52e598f6fc6b1eae74888";
+    hash = "sha256-GiptGi2OZy6eiDa/g8GwPVZpDd8QUxZuzaEy+9J+v9I=";
+  };
 
-    # vendorHash = lib.fakeHash;
-    vendorHash = "sha256-alC4/2wTbjJYWGzTDTgQweOicN3xSqfnncok/j16+0E=";
+  # vendorHash = lib.fakeHash;
+  /*
+  vendorHash = "sha256-alC4/2wTbjJYWGzTDTgQweOicN3xSqfnncok/j16+0E=";
+  */
+  vendorHash = "sha256-72Vjrl/v4rKojv3BeACcEiOyWK4c9KCyzxJtfNx/7UE=";
 
-    CGO_ENABLED = 1;
-    ldflags = [ "-s" "-w" ];
+  CGO_ENABLED = 0;
+  ldflags = ["-s" "-w"];
 
-    tags = [ "torrent" "bittorrent" "anime" ];
-    proxyVendor = true;
+  tags = ["torrent" "bittorrent" "anime"];
+  # proxyVendor = true;
+  #
+  # buildPhase = ''
+  #     go mod vendor
+  #     go build -o toru ./cmd/toru
+  # '';
+  #
+  # installPhase = ''
+  #     mkdir -p $out/bin
+  #     mv toru $out/bin
+  # '';
 
-    buildPhase = ''
-        go mod vendor
-        go build -o toru ./cmd/toru
-    '';
-
-    installPhase = ''
-        mkdir -p $out/bin
-        mv toru $out/bin
-    '';
-
-    meta = with lib; {
-        homepage = "https://github.com/sweetbbak/toru";
-        description = "Stream anime from the command line with the power of torrents";
-        license = licenses.mit;
-        maintainers = with maintainers; [ sweetbbak ];
-        mainProgram = "toru";
-    };
+  meta = with lib; {
+    homepage = "https://github.com/sweetbbak/toru";
+    description = "Bittorrent streaming CLI tool. Stream anime torrents, real-time with no waiting for downloads";
+    license = licenses.mit;
+    maintainers = with maintainers; [sweetbbak];
+    mainProgram = "toru";
+  };
 }
