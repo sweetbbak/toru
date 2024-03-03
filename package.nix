2ee8c18d7e7a933199c52a2ccf813266ff78655a
Meili C <sweetbabyalaska@gmail.com>
2024-03-02 16:15:46 -0900

    new dev nix pkg update

--- package.nix
+++ package.nix
@@ -3,7 +3,6 @@
   fetchFromGitHub,
   buildGoModule,
 }:
-
 buildGoModule rec {
   pname = "toru";
   version = "0.1";
@@ -12,30 +11,13 @@ buildGoModule rec {
     owner = "sweetbbak";
     repo = "toru";
     rev = "c5f6134b42d6dc8fd3f52e598f6fc6b1eae74888";
-    hash = "sha256-GiptGi2OZy6eiDa/g8GwPVZpDd8QUxZuzaEy+9J+v9I=";
+    hash = "sha256-6jZnuE2xNF0lrYVjBXTBGQCkT0vxPzHlmbnRDLPcglE=";
   };
 
-  # vendorHash = lib.fakeHash;
-  /*
-  vendorHash = "sha256-alC4/2wTbjJYWGzTDTgQweOicN3xSqfnncok/j16+0E=";
-  */
-  vendorHash = "sha256-72Vjrl/v4rKojv3BeACcEiOyWK4c9KCyzxJtfNx/7UE=";
-
+  vendorHash = "sha256-bmG2qBlyN6aYdWvplXClylwCk2pDfr30w0ztgUXP71g=";
   CGO_ENABLED = 0;
   ldflags = ["-s" "-w"];
-
   tags = ["torrent" "bittorrent" "anime"];
-  # proxyVendor = true;
-  #
-  # buildPhase = ''
-  #     go mod vendor
-  #     go build -o toru ./cmd/toru
-  # '';
-  #
-  # installPhase = ''
-  #     mkdir -p $out/bin
-  #     mv toru $out/bin
-  # '';
 
   meta = with lib; {
     homepage = "https://github.com/sweetbbak/toru";
