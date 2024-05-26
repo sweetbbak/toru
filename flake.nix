b0f59c722c88efedb7b88b43ff161421d16d66c1
Meili C <sweetbabyalaska@gmail.com>
2024-05-25 18:30:30 -0800

    [toru] update pkgs

--- flake.nix
+++ flake.nix
@@ -1,16 +1,49 @@
+# {
+#   inputs.nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+#   outputs = {nixpkgs, ...}: {
+#     packages.x86_64-linux = let
+#       pkgs = nixpkgs.legacyPackages.x86_64-linux;
+#     in {
+#       default = pkgs.callPackage ./package.nix {};
+#     };
+#
+#     packages.aarch64-darwin = let
+#       pkgs = nixpkgs.legacyPackages.x86_64-linux;
+#     in {
+#       default = pkgs.callPackage ./package.nix {};
+#     };
+#   };
+# }
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
-  outputs = {nixpkgs, ...}: {
-    packages.x86_64-linux = let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in {
-      default = pkgs.callPackage ./package.nix {};
-    };
+  description = "Flake utils demo";
 
-    packages.aarch64-darwin = let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in {
-      default = pkgs.callPackage ./package.nix {};
-    };
-  };
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+        go = pkgs.go;
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            go
+            pkgs.gopls
+            pkgs.go-tools
+          ];
+          shellHook = ''
+            export CGO_ENABLED=0
+            export GOOS=${system}
+            export GOARCH=${system}
+          '';
+        };
+
+        # default = pkgs.callPackage ./nix/package.nix {};
+        defaultPackage = pkgs.callPackage ./nix/package.nix {};
+      }
+    );
 }
--- nix/package.nix
+++ nix/package.nix
@@ -5,13 +5,13 @@
 }:
 buildGoModule {
   pname = "toru";
-  version = "0.2";
+  version = "0.3";
 
   src = fetchFromGitHub {
     owner = "sweetbbak";
     repo = "toru";
-    rev = "cf1729a116927b5ffc28eb99360628a27097fbe6";
-    hash = "sha256-Na4JJ4RvghY/cx1H6XwMoP6963hobZyvZqhoKsGPDxs=";
+    rev = "bb0d81894f59bf4c849502bd9cdab42ba79fdffe";
+    hash = "sha256-oIPXODgXO5HvDLGxUT43IibkznNbJIQA5DvC3QDcWRw=";
   };
 
   vendorHash = "sha256-04fHwblTspzecnTizUlFqLwtcnBsHrFRvLX1eSXztRI=";
@@ -19,6 +19,22 @@ buildGoModule {
   CGO_ENABLED = 0;
   ldflags = ["-s" "-w"];
   tags = ["torrent" "bittorrent" "anime"];
+
+  buildPhase = ''
+    go build -ldflags="-s -w" ./cmd/toru
+  '';
+
+  installPhase = ''
+    mkdir -p $out/share/zsh/site-functions
+    cp completion/_toru $out/share/zsh/site-functions/_toru
+
+    mkdir -p $out/etc/bash_completion.d
+    cp completion/_toru_bash $out/etc/bash_completion.d/_toru
+
+    mkdir -p $out/bin
+    ls
+    mv toru $out/bin
+  '';
 
   meta = with lib; {
     homepage = "https://github.com/sweetbbak/toru";
